cbb6251a61708108b9d5da13714a6a5c5ca18f58
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-30 22:19:35 +0100

    Improved functionality, props and reusability

--- React Dropdown/src/App.jsx
+++ React Dropdown/src/App.jsx
@@ -1,31 +1,15 @@
-import React, { useState } from 'react'
-import DropdownMenu from './DropdownMenu'
+import React from 'react'
+import DropdownMenu from './components/dropdown'
+import DropdownContent from './components/dropdown/dropdownContent'
 
 
 export default function App() {
-
-  const [ dropdown, setDropDown ] = useState(false)
-
-  const handleMouseEnter = () => {
-    setDropDown(true)
-  }
-
-  const handleMouseLeave = () => {
-    setDropDown(false)
-  }
-
   return (
-    <div className='app'>
-      <nav>
-        <div
-          className="menu"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <h3>Dropdown Menu</h3>
-          {dropdown && <DropdownMenu />}
-        </div>
-      </nav>
-    </div>
+    <>
+      <DropdownMenu buttonContent="Explore " >
+        <DropdownContent />
+      </DropdownMenu>
+    </>
   )
 }
+
