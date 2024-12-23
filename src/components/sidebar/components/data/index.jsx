7577fd26c439745cfa33c3f736e69cba66aee661
5thAttemptCode <henrycodes@outlook.com>
2024-12-23 22:50:52 +1300

    added color variable

--- src/components/sidebar/components/data/index.jsx
+++ src/components/sidebar/components/data/index.jsx
@@ -1,37 +1,39 @@
 import React from 'react'
 import { ArticleNyTimes, Barcode, ChatCenteredDots, Headset, House, UsersThree } from 'phosphor-react'
 
+
 const size = 22
+const color = "#ffffff"
 
 export const SideData = [
   {
     title: 'Home',
     path: '/',
-    icon: <House size={size} />
+    icon: <House size={size} color={color} />
   },
   {
     title: 'Reports',
     path: '/reports',
-    icon: <ArticleNyTimes size={size} />    
+    icon: <ArticleNyTimes size={size} color={color} />    
   },
   {
     title: 'Products',
     path: '/products',
-    icon: <Barcode size={size} />
+    icon: <Barcode size={size} color={color} />
   },
   {
     title: 'Team',
     path: '/team',
-    icon: <UsersThree size={size} />
+    icon: <UsersThree size={size} color={color} />
   },
   {
     title: 'Messages',
     path: '/messages',
-    icon: <ChatCenteredDots size={size} />
+    icon: <ChatCenteredDots size={size} color={color} />
   },
   {
     title: 'Support',
     path: '/support',
-    icon: <Headset size={size} />
+    icon: <Headset size={size} color={color} />
   }
 ]
